87429d68a9d8eedcbb8a1b7d5abf3adac3905814
LuxDie <ezequielcicala@hotmail.com>
2015-11-04 16:18:48 -0300

    Add interface elements and styles.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,10 @@
 // jshint devel:true
 (function () {
-// Create a new Carousel with jQuery or Zepto.
+
   var carousel = $('.myCarousel').carousel({
     limitPerPage: 3
   });
+
+  var tabs = $(selector).Tabs();
+
 })();
